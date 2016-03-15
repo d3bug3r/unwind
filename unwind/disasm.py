08e48d3188b3be849b1348547667ecb8053b4615
Alexandr <alex.s@easyuni.com>
2016-03-15 07:11:57 -0400

    added all primitive types, func call support

--- unwind/disasm.py
+++ unwind/disasm.py
@@ -167,6 +167,299 @@ _TYPE_UNICODE = ord('u')
 _TYPE_SET = ord('<')
 _TYPE_FROZEN_SET = ord('>')
 
+INDENT = "    "
+
+class _PyObject():
+
+
+    def __init__(self, val=0, print_raw=0, skip_it=0):
+        self.val = val
+        self.print_raw = print_raw
+        self.skip_it = skip_it
+
+    def __repr__(self):
+        if self.print_raw:
+            return "{}".format(self.val)
+        return self.val.__repr__()
+
+class _FuncCall():
+    
+
+    def __init__(self, func_name, pargs, kargs, pargs_ext=None, kargs_ext=None):
+        self.func_name = func_name
+        self.pargs = pargs
+        self.kargs = kargs
+        all_args = [_.__repr__() for _ in self.pargs]
+        if pargs_ext:
+            all_args.append("*{}".format(pargs_ext.__repr__()))
+        all_args.extend([k.val + "=" + str(v) for k,v in self.kargs.iteritems()])
+        if kargs_ext:
+            all_args.append("**{}".format(kargs_ext.__repr__()))
+        self.args_repr = ", ".join(all_args)
+        self.val =  ''.join([self.func_name, "(", self.args_repr, ")"])
+
+    def __repr__(self):
+        return self.val
+
+class _MathOperation(): 
+
+
+    def __init__(self, val1, val2, operation):
+        self.val1 = val1
+        self.val2 = val2
+        self.operation = operation
+
+    def __repr__(self):
+        return "({} {} {})".format(self.val1, self.operation, self.val2)
+
+class _Assigment():
+
+
+    def __init__(self, name, val, indent=0):
+        self.name = name
+        self.val = val
+        self.indent = indent
+
+    def __repr__(self):
+        return self.indent *_INDENT + "{} = {}".format(self.name, self.val)
+
+
+class _CommandHandler():
+
+
+    def __init__(self):
+        self.stack = list()
+        self.result = list()
+
+        self.indent_count = 0
+        self.INDENT_SIZE = "    "
+
+        self.slice_expression = [op.SLICE_0, op.SLICE_1, op.SLICE_2, op.SLICE_3]
+        self.slice_statemant = [op.STORE_SLICE_0, op.STORE_SLICE_1, op.STORE_SLICE_2,
+                                op.STORE_SLICE_3, op.DELETE_SLICE_0, op.DELETE_SLICE_1,
+                                op.DELETE_SLICE_2, op.DELETE_SLICE_3, op.STORE_SUBSCR,
+                                op.DELETE_SUBSCR]
+        self.math_operations = [op.BINARY_POWER, op.BINARY_MULTIPLY, op.BINARY_FLOOR_DIVIDE,
+                                op.BINARY_TRUE_DIVIDE, op.BINARY_MODULO, op.BINARY_ADD,
+                                op.BINARY_SUBTRACT, op.BINARY_LSHIFT, op.BINARY_RSHIFT,
+                                op.BINARY_AND, op.BINARY_XOR, op.BINARY_OR,
+
+                                op.INPLACE_POWER, op.INPLACE_MULTIPLY, op.INPLACE_FLOOR_DIVIDE,
+                                op.INPLACE_TRUE_DIVIDE, op.INPLACE_MODULO, op.INPLACE_ADD,
+                                op.INPLACE_SUBTRACT, op.INPLACE_LSHIFT, op.INPLACE_RSHIFT,
+                                op.INPLACE_AND, op.INPLACE_XOR, op.INPLACE_OR
+                                ]
+
+        self.function_call = [op.CALL_FUNCTION, op.CALL_FUNCTION_VAR,
+                                op.CALL_FUNCTION_KW, op.CALL_FUNCTION_VAR_KW]
+
+        self.pwr_operations = [op.BINARY_POWER, op.INPLACE_POWER]
+        self.mul_operations = [op.BINARY_MULTIPLY, op.INPLACE_MULTIPLY]
+        self.floor_div_operations = [op.BINARY_FLOOR_DIVIDE, op.INPLACE_FLOOR_DIVIDE]
+        self.true_div_operations = [op.BINARY_TRUE_DIVIDE, op.INPLACE_TRUE_DIVIDE]
+        self.mod_operations = [op.BINARY_MODULO, op.INPLACE_MODULO]
+        self.add_operations = [op.BINARY_ADD, op.INPLACE_ADD]
+        self.sub_operations = [op.BINARY_SUBTRACT, op.INPLACE_SUBTRACT]
+        self.shl_operations = [op.BINARY_LSHIFT, op.INPLACE_LSHIFT]
+        self.shr_operations = [op.BINARY_RSHIFT, op.INPLACE_RSHIFT]
+        self.and_operations = [op.BINARY_AND, op.INPLACE_AND]
+        self.xor_operations = [op.BINARY_XOR, op.INPLACE_XOR]
+        self.or_operations = [op.BINARY_OR, op.INPLACE_OR]
+
+    def indent(self):
+        return self.indent_count * self.INDENT_SIZE
+
+    def inc_indent(self):
+        self.indent_count += 1
+
+    def dec_indent(self):
+        self.indent_count -= 1
+
+    def pop(self):
+        return self.stack.pop()
+
+    def pop_n(self, n):
+        return [self.pop() for _ in range(n)]
+
+    def pop_n_rev(self, n):
+        temp = self.pop_n(n)
+        temp.reverse()
+        return temp
+
+    def push(self, val):
+        self.stack.append(val)
+
+    def top(self):
+        return self.stack[-1]
+
+    def rotate(self, n, l):
+        return l[n:] + l[:n]
+
+    def pop_n_rev_rot(self, shiftn, popn):
+        return self.rotate(shiftn, self.pop_n_rev(popn))
+
+
+    def read_kargs(self, num):
+        kargs = dict()
+        for _ in range(num):
+            val = self.pop()
+            key = self.pop()
+            kargs[key] = val
+        return kargs
+
+    def read_pargs(self, num):
+        pargs = list()
+        for _ in range(num):
+            pargs.insert(0, self.pop())
+        return pargs
+
+    #TODO add loops, if, function declaration, class, declaration
+    def handle(self, opcode, arg):
+        print(opcode, arg, self.stack)
+        if opcode == op.NOP: pass
+        elif opcode == op.POP_TOP: self.pop()
+        elif opcode == op.ROT_TWO: self.stack[-2:] = self.rotate(2, self.stack[-2:])
+        elif opcode == op.ROT_THREE: self.stack[-3:] = self.rotate(3, self.stack[-3:])
+        elif opcode == op.ROT_FOUR: self.stack[-4:] = self.rotate(4, self.stack[-4:])
+
+        elif opcode == op.UNARY_POSITIVE: pass
+        elif opcode == op.UNARY_NEGATIVE: self.top().val = "-" + self.top().val
+        elif opcode == op.UNARY_NOT: self.top().val = "not " + self.top().val
+        elif opcode == op.UNARY_INVERT: self.top().val = "~" + self.top().val
+
+        elif opcode == op.LOAD_CONST: self.push(_PyObject(arg))
+        elif opcode == op.LOAD_NAME: self.push(_PyObject(arg, print_raw=1))
+
+        elif opcode == op.BUILD_LIST: self.push(self._make_list(arg))
+        elif opcode == op.BUILD_SET: self.push(set(self._make_list(arg)))
+        elif opcode == op.BUILD_TUPLE: self.push(tuple(self._make_list(arg)))
+
+        elif opcode == op.BUILD_MAP: self.push(dict())
+        elif opcode == op.STORE_MAP:
+            key = self.pop()
+            value = self.pop()
+            self.stack[-1][key] = value
+
+        elif opcode in self.math_operations:
+            val2 = self.pop()
+            val1 = self.pop()
+            
+            if opcode in self.pwr_operations: cmd = "**"
+            elif opcode in self.mul_operations: cmd = "*"
+            elif opcode in self.floor_div_operations: cmd = "//"
+            elif opcode in self.true_div_operations: cmd = "/"
+            elif opcode in self.mod_operations: cmd = "%"
+            elif opcode in self.add_operations: cmd = "+"
+            elif opcode in self.sub_operations: cmd = "-"
+            elif opcode in self.shl_operations: cmd = "<<"
+            elif opcode in self.shr_operations: cmd = ">>"
+            elif opcode in self.and_operations: cmd = "&"
+            elif opcode in self.xor_operations: cmd = "^"
+            elif opcode in self.or_operations: cmd = "|"
+
+            self.push(_MathOperation(val1, val2, cmd))
+
+
+        elif opcode in self.slice_expression:
+            if opcode == op.SLICE_0: val = "{}[:]".format(*self.pop_n_rev(1))
+            elif opcode == op.SLICE_1: val = "{}[{}:]".format(*self.pop_n_rev(2))
+            elif opcode == op.SLICE_2: val = "{}[:{}]".format(*self.pop_n_rev(2))
+            elif opcode == op.SLICE_3: val = "{}[{}:{}]".format(*self.pop_n_rev(3))
+
+            self.push(_PyObject(val, print_raw=1))
+
+        elif opcode in self.slice_statemant:
+            if opcode == op.STORE_SLICE_0: var = "{}[:] = {}".format(*self.pop_n(2))
+            elif opcode == op.STORE_SLICE_1: var = "{}[{}:] = {}".format(*self.pop_n_rev_rot(1, 3))
+            elif opcode == op.STORE_SLICE_2: var = "{}[:{}] = {}".format(*self.pop_n_rev_rot(1, 3))
+            elif opcode == op.STORE_SLICE_3: var = "{}[{}:{}] = {}".format(*self.pop_n_rev_rot(1,4))
+
+            elif opcode == op.DELETE_SLICE_0: var = "del {}[:]".format(self.pop())
+            elif opcode == op.DELETE_SLICE_1: var = "del {}[{}:]".format(*self.pop_n_rev(2))
+            elif opcode == op.DELETE_SLICE_2: var = "del {}[{}:]".format(*self.pop_n_rev(2))
+            elif opcode == op.DELETE_SLICE_3: var = "del {}[{}:{}]".format(*self.pop_n_rev(3))
+
+            elif opcode == op.DELETE_SUBSCR: var = "del {}[{}]".format(*pop_n_rev(2))
+            elif opcode == op.STORE_SUBSCR: var = "{}[{}] = {}".format(*self.pop_n_rev_rot(1, 3))
+            self.result.append(self.indent() + var)
+        elif opcode == op.BINARY_SUBSCR:
+            tos = self.pop()
+            tos1 = self.pop()
+            self.push("{}[{}]".format(tos1, tos))
+
+
+        # TODO fix module import 
+        elif opcode == op.IMPORT_NAME:
+            _ = self.pop()
+            self.push(arg)
+
+        elif opcode == op.IMPORT_STAR:
+            self.result.append(self.indent() + "from {} import *".format(self.pop()))
+        elif opcode == op.IMPORT_FROM:
+            self.push("{}.{}".format(self.top(), arg))
+
+
+        elif opcode == op.PRINT_ITEM_TO: self.result.append(self.indent() + "print >> {}, {}".format(self.top(), self.pop()))
+        elif opcode == op.PRINT_NEWLINE_TO: self.result.append(self.indent() + "print >> {}".format(self.pop()))
+        elif opcode == op.PRINT_ITEM: self.result.append(self.indent() + "print({}, end='')".format(self.pop()))
+        elif opcode == op.PRINT_NEWLINE: self.result.append(self.indent() + "print()")
+
+        elif opcode in self.function_call:
+            pargc = arg & 0xff
+            kargc = (arg >> 8) & 0xff
+
+            ext_list = ext_dict = None
+            if opcode == op.CALL_FUNCTION: pass
+            elif opcode == op.CALL_FUNCTION_VAR: ext_list = self.pop()
+            elif opcode == op.CALL_FUNCTION_KW: ext_dict = self.pop()
+            elif opcode == op.CALL_FUNCTION_VAR_KW: ext_dict = self.pop(); ext_list = self.pop()
+
+            kargs = self.read_kargs(kargc)
+            pargs = self.read_pargs(pargc)
+            func_name = self.pop().__repr__()
+
+            self.push(_FuncCall(func_name, pargs, kargs, ext_list, ext_dict))
+
+        elif opcode == op.MAKE_FUNCTION:
+            code_obj = self.pop().val
+
+            func_name = code_obj.co_name
+            argc = code_obj.co_argcount
+            kwargc = arg
+            local_vars = code_obj.co_varnames
+
+            all_args = list() 
+            all_args.extend(local_vars[:argc-kwargc])
+            all_args.extend(["{}={}".format(key, val) for key,val in zip(local_vars[argc-kwargc:argc], self.pop_n_rev(2))])
+
+            self.result.append(self.indent() + "def {}({}):".format(func_name, ", ".join(all_args)))
+            #self.inc_indent()
+
+            #no need to STORE_NAME the function declaration
+            self.push(_PyObject(skip_it=1))
+
+        elif opcode == op.RETURN_VALUE:
+            if arg:
+                self.result.append(self.indent() + "return {}".format(arg))
+            self.dec_indent()
+        elif opcode == op.DELETE_NAME: self.result.append(self.indent() + "del {}".format(arg))
+        elif opcode == op.STORE_NAME:
+            tos = self.pop()
+            if hasattr(tos, "skip_it") and not tos.skip_it:
+                self.result.append(self.indent() + _Assigment(arg, tos).__repr__())
+
+    def _make_list(self, argc):
+        temp = temp = [self.stack.pop() for _ in range(argc)]
+        temp.reverse()
+        return temp
+
+    def print_result(self):
+        for command in self.result:
+            print(command)
+
+
+
+
 # Holds intermediate state useful during disassembly. Only the disassemble()
 # method is meant to be called directly.
 class _Disassembler:
@@ -278,6 +571,7 @@ class _Disassembler:
             # Start disassembly
             argument = 0
             i = 0
+            handler = _CommandHandler()
             while i < len(co.co_code):
                 offset = i
                 opcode = op.from_bytecode(co.co_code[i], self.magic)
@@ -319,8 +613,10 @@ class _Disassembler:
 
                 # Record disassembled opcode
                 co.opcodes.append(Opcode(offset, i - offset, opcode, arg))
+                handler.handle(opcode, arg)
                 argument = 0
 
+            handler.print_result()
             return co
 
         else:
